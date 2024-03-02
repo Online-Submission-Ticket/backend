af27473b7c95f1c118802638c1b12565598090e2
Kavita <thetekavita77@gmail.com>
2024-03-02 15:57:52 +0530

    working register and login

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,56 +67,56 @@ export const registerController = async (req,res) =>{
 
 
 //POST LOGIN
+// POST LOGIN
 export const loginController = async (req, res) => {
-    try {
+  try {
       const { email, password } = req.body;
-      //validation
+      // validation
       if (!email || !password) {
-        return res.status(400).send({
-          success: false,
-          message: "Invalid email or password",
-        });
+          return res.status(400).send({
+              success: false,
+              message: "Invalid email or password",
+          });
       }
-      //check user
-      const user = await admin.findOne({ email });
-      if (!user) {
-        return res.status(200).send({
-          success: false,
-          message: "Email is not registerd!! Please sign up",
-        });
+      // check user
+      const foundUser = await user.findOne({ email });
+      if (!foundUser) {
+          return res.status(200).send({
+              success: false,
+              message: "Email is not registered!! Please sign up",
+          });
       }
 
-       
-      const match = await comparePassword(password, user.password);
+      const match = await comparePassword(password, foundUser.password);
       if (!match) {
-        return res.status(200).send({
-          success: false,
-          message: "Invalid Password please enter correct password",
-        });
+          return res.status(200).send({
+              success: false,
+              message: "Invalid Password please enter correct password",
+          });
       }
-      //token
-      const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "7d",
+      // token
+      const token = await JWT.sign({ _id: foundUser._id }, process.env.JWT_SECRET, {
+          expiresIn: "7d",
       });
       res.status(200).send({
-        success: true,
-        message: "login successfully",
-        user: {
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          phone: user.phone,
-          roll_no: user.roll_no,
-          division: user.division,
-        },
-        token,
+          success: true,
+          message: "Login successfully",
+          user: {
+              _id: foundUser._id,
+              name: foundUser.name,
+              email: foundUser.email,
+              phone: foundUser.phone,
+              roll_no: foundUser.roll_no,
+              division: foundUser.division,
+          },
+          token,
       });
-    } catch (error) {
+  } catch (error) {
       console.log(error);
       res.status(500).send({
-        success: false,
-        message: "Error in login",
-        error,
+          success: false,
+          message: "Error in login",
+          error,
       });
-    }
-  };
+  }
+};
